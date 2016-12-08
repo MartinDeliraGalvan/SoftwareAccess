fab7f2d169cde39b1cab4d4bb5611be125ba24d7
Martin Delira <mdelira@miners.utep.edu>
2016-12-07 19:43:53 -0700

    Updated pom.xml artifact and name from "helloworld" to access

--- Access/src/main/java/net/viralpatel/spring/controller/LoginController.java
+++ Access/src/main/java/net/viralpatel/spring/controller/LoginController.java
@@ -1,10 +1,18 @@
 package net.viralpatel.spring.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+	
+	@GetMapping("/login")
+	public String login(Model model) {
+	
+		return "login";
+	}
+
 	
 	
 	
